7b027f1997b26274aad6d0d13df6594a5526430c
MelodyFss <melodyponczko@gmail.com>
2023-10-23 18:20:35 -0300

    Adjusting colors

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*-----------------------------------------typing animation----------------------------------*/
+var typed = new Typed(".typing",{
+    strings : ["Front-end developer","Full Stack Web developer","UX/UI designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
