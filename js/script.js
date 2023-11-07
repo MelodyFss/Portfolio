305e29ea442449d42a2a141441956763c2a16442
MelodyFss <melodyponczko@gmail.com>
2023-11-06 22:38:31 -0300

    links

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*-----------------------------------------typing animation----------------------------------*/
 var typed = new Typed(".typing",{
-    strings : ["Front-end developer","Full Stack Web developer","UX/UI designer"],
+    strings : ["","Front-end developer","Full Stack Web developer","UX/UI designer"],
     typeSpeed:100,
     backSpeed:60,
     loop:true
